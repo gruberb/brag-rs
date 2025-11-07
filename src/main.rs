3148e236ef0774e98a6e28c3ef3b9900db7db00a
Tif Tran <ttran@mozilla.com>
2025-11-07 10:55:50 -0800

    add GET and DELETE

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
 use axum::{
     routing::{get, post},
     http::StatusCode,
     Json, Router,
 };
+use axum::extract::{Path, State};
+use axum::routing::delete;
 use serde::{Deserialize, Serialize};
 use chrono::prelude::*;
+use tracing::info;
+
+type Db = Arc<RwLock<HashMap<i64, Entry>>>;
 
 #[tokio::main]
 async fn main() {
@@ -12,12 +19,15 @@ async fn main() {
     tracing_subscriber::fmt::init();
     tracing::info!("Starting server...");
 
+    let db = Db::default();
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/entries", post(create_entry));
+        .route("/", get(get_entries))
+        // `POST /entries` goes to `create_entry`
+        .route("/entries", post(create_entry))
+        .route("/entries/{id}", delete(delete_entry) )
+        .with_state(db);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -25,11 +35,15 @@ async fn main() {
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn get_entries(State(db): State<Db>) -> (StatusCode, Json<Vec<Entry>>){
+    let m = db.read().unwrap();
+    let result = m.values().cloned().collect();
+    (StatusCode::OK, Json(result))
+
 }
 
 async fn create_entry(
+    State(db): State<Db>,
     Json(payload): Json<CreateEntry>,
 ) -> (StatusCode, Json<Entry>) {
     // insert your application logic here
@@ -40,18 +54,28 @@ async fn create_entry(
        ..Default::default()
     };
 
+    let mut w = db.write().unwrap();
+    w.insert(entry.date, entry.clone());
+
     // this will be converted into a JSON response
     // with a status code of `201 Created`
     (StatusCode::CREATED, Json(entry))
 }
 
-#[derive(Deserialize, Serialize)]
+async fn delete_entry(Path(id): Path<i64>, State(db): State<Db>) -> StatusCode {
+    let mut w = db.write().unwrap();
+    w.remove(&id);
+    info!("{w:?}");
+    StatusCode::FOUND
+
+}
+#[derive(Deserialize, Serialize, Clone, Debug)]
 struct Collaborator {
     name: String,
     team: String
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 struct Entry {
     description: String,
     collaborators: Vec<Collaborator>,
@@ -70,7 +94,7 @@ impl Default for Entry {
     }
 }
 
-// the input to our `create_user` handler
+// the input to our `create_entry` handler
 #[derive(Deserialize)]
 struct CreateEntry {
     description: String,
@@ -78,18 +102,3 @@ struct CreateEntry {
     link: Option<String>,
 }
 
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
-
-impl Default for User {
-    fn default() -> Self {
-        Self {
-            id: 0,
-            username: "".to_string(),
-        }
-    }
-}
